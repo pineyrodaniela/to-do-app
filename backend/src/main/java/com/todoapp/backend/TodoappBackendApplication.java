2f09b5b8d04a70f27bcc56283da6e897595d8440
Daniela <pineyrodaniela@hotmail.com>
2026-02-04 11:09:06 -0300

    pom configuration project dependencies, maven config

---
+++ backend/src/main/java/com/todoapp/backend/TodoappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.todoapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/todoapp/backend/TodoappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
